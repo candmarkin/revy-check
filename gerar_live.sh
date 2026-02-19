67788cae3f2c639015655f00bfbb3b2e4864f5de
Marcos Luan Candido <marcoscandido180@gmail.com>
2026-02-19 10:50:44 -0300

    Hotfix

--- gerar_live.sh
+++ gerar_live.sh
@@ -10,9 +10,6 @@ echo "[2/3] Removendo squashfs antigo..."
 rm -f $LIVE/filesystem.squashfs
 
 echo "[3/3] Gerando squashfs com excludes..."
-mksquashfs / $LIVE/filesystem.squashfs \
-    -comp xz \
-    -wildcards \
-    -e proc sys dev tmp run mnt media lost+found live
+mksquashfs / $LIVE/filesystem.squashfs -e /proc/* /dev/* /sys/* /mnt/* /tmp/* -comp xz -b 1048576 -Xdict-size 100% -Xbcj x86 -always-use-fragments -noappend -no-wildcards
 
 echo "PRONTO -> $LIVE/filesystem.squashfs"
