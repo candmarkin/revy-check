cea6625b1a134b0174b6869a212b5d02d9ea0ed1
Marcos Luan Candido <marcoscandido180@gmail.com>
2026-02-19 07:49:55 -0300

    Hotfix

--- install_debian.sh
+++ install_debian.sh
@@ -31,10 +31,8 @@ apt-get install -y \
     python3-opencv \
     python3-numpy \
     python3-pulsectl \
-    python3-sounddevice \
     python3-tk \
     cifs-utils \
-    lsusb \
     usbutils \
     net-tools \
     alsa-utils \
@@ -43,7 +41,7 @@ apt-get install -y \
 
 echo ""
 echo "📦 Instalando dependências Python adicionais via pip..."
-pip3 install --break-system-packages mysql-connector-python ntplib || pip3 install mysql-connector-python ntplib
+pip3 install --break-system-packages -r requirements.txt || pip3 install mysql-connector-python ntplib
 
 echo ""
 echo "✅ Dependências instaladas!"
