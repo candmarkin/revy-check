fd8534f07e06de092bb6a763658864eceba59bb1
Marcos Luan Candido <marcoscandido180@gmail.com>
2026-02-19 09:09:35 -0300

    Hotfix

--- gerar_live.sh
+++ gerar_live.sh
@@ -1,46 +1,18 @@
 #!/bin/sh
 set -e
 
-WORK=/tmp/live-build
-ROOTFS=$WORK/rootfs
 LIVE=/live
 
-echo "[1/6] Preparando pastas..."
-rm -rf "$WORK"
-mkdir -p "$ROOTFS"
+echo "[1/3] Preparando pasta de destino..."
 mkdir -p "$LIVE"
 
-echo "[2/6] Copiando root atual..."
-rsync -a \
-    --delete \
-    --exclude=/proc \
-    --exclude=/sys \
-    --exclude=/dev \
-    --exclude=/tmp \
-    --exclude=/run \
-    --exclude=/mnt \
-    --exclude=/media \
-    --exclude=/lost+found \
-    --exclude=/live \
-    / "$ROOTFS"
-
-echo "[3/6] Limpando arquivos temporarios..."
-rm -rf $ROOTFS/var/log/*
-rm -rf $ROOTFS/var/cache/*
-rm -rf $ROOTFS/tmp/*
-
-echo "[4/6] Recriando dirs runtime..."
-mkdir -p $ROOTFS/proc
-mkdir -p $ROOTFS/sys
-mkdir -p $ROOTFS/dev
-mkdir -p $ROOTFS/run
-mkdir -p $ROOTFS/tmp
-chmod 1777 $ROOTFS/tmp
-
-echo "[5/6] Removendo squashfs antigo..."
+echo "[2/3] Removendo squashfs antigo..."
 rm -f $LIVE/filesystem.squashfs
 
-echo "[6/6] Gerando squashfs..."
-mksquashfs $ROOTFS $LIVE/filesystem.squashfs -comp xz -wildcards
+echo "[3/3] Gerando squashfs com excludes..."
+mksquashfs / $LIVE/filesystem.squashfs \
+    -comp xz \
+    -wildcards \
+    -e proc sys dev tmp run mnt media lost+found live
 
 echo "PRONTO -> $LIVE/filesystem.squashfs"
